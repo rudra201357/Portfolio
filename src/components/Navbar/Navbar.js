b17a5045d0fa3c79f1232b9912254ed1ece1b234
RUDRADEB PAL <deb47250@gmail.com>
2026-05-01 15:19:22 +0530

    Navbar Updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,35 @@
-import './Navbar.css';
+import { useEffect, useState } from "react";
+import "./Navbar.css";
+
 function Navbar() {
+  const [showName, setShowName] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const homeSection = document.getElementById("home");
+      const rect = homeSection.getBoundingClientRect();
+
+      // If Home section is visible → hide name
+      if (rect.top <= 100 && rect.bottom >= 100) {
+        setShowName(false);
+      } else {
+        setShowName(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="nav">
-<p className='nav-name' >Rudradeb Pal</p>
-   <ul>
+     <p className={`nav-name ${showName ? "show" : "hide"}`}>
+  Rudradeb Pal <br />
+ <span id="role">SW Developer</span> 
+</p>
+
+      <ul>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#skills">Skills</a></li>
